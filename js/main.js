ebf7ae5a3e2184f69b3b4c4c65b537f6c2dd0865
Junhyeokss <118700002+Junhyeokss@users.noreply.github.com>
2022-12-28 17:41:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 $(function () {
   $(".burger_slide").slick({
     arrows: false,
-    autoplay: false,
+    autoplay: true,
     autoplaySpeed: 2000,
     pauseOnHover: false,
     pauseOnFocus: false,
     dots: true,
+    useTransform: false,
   });
   $(".burger_slide").on("afterChange", function (e, s, c) {
     $(".slick-dots li").eq(c).addClass("on").siblings().removeClass("on");
